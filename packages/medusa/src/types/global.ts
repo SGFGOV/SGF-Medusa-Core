3f4afba4532691bfd33b989a4674b4d7b2096547
SGF <SGF@sourcegoodfood.com>
2022-06-18 00:53:04 +0000

    feat:refactored to have single point of configuration promise resolution

--- packages/medusa/src/types/global.ts
+++ packages/medusa/src/types/global.ts
@@ -37,19 +37,13 @@ export type Logger = _Logger & {
   warn: (msg: string) => void
 }
 
-<<<<<<< HEAD
 export type DatabaseTlsOptions =
 {
   ca:string|undefined
   rejectUnauthorized:boolean|undefined
-=======
-export type TlsOptions = {
-  ca: string | undefined
-  rejectUnauthorized: boolean | undefined
->>>>>>> 57c2bc6a5 (chore: clean up database host configuration & make config promise resolution single point)
 }
 
-export type HostConfig =
+export type DatabaseHostConfig =
   | {
       database?: string
       password?: string | (() => string) | (() => Promise<string>)
@@ -71,16 +65,10 @@ export type ConfigModule = {
     cookie_secret?: string
     database_host?: string
     database_port?: number
-<<<<<<< HEAD
     database_ssl?: DatabaseTlsOptions
     database_username?: string
     database_password?: string | (() => string) | (() => Promise<string>)
     database_database?: string
-=======
-    database_ssl?: TlsOptions
-    database_username?: string
-    database_password?: string | (() => string) | (() => Promise<string>)
->>>>>>> 57c2bc6a5 (chore: clean up database host configuration & make config promise resolution single point)
 
     database_url?: string
     database_type: string
