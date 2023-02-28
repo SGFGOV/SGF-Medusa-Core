dca158811582491b3fa384bea26f6e4070c5ca56
SGF <SGF@sourcegoodfood.com>
2023-02-28 09:09:42 +0000

    fix: build

--- packages/medusa/src/types/global.ts
+++ packages/medusa/src/types/global.ts
@@ -44,6 +44,57 @@ export type Logger = _Logger & {
   warn: (msg: string) => void
 }
 
+export enum MODULE_SCOPE {
+  INTERNAL = "internal",
+  EXTERNAL = "external",
+}
+
+export enum MODULE_RESOURCE_TYPE {
+  SHARED = "shared",
+  ISOLATED = "isolated",
+}
+
+/*export type ConfigurableModuleDeclaration = {
+  scope: MODULE_SCOPE.INTERNAL
+  resources: MODULE_RESOURCE_TYPE
+  resolve?: string
+  options?: Record<string, unknown>
+}*/
+/*
+| {
+    scope: MODULE_SCOPE.external
+    server: {
+      type: "built-in" | "rest" | "tsrpc" | "grpc" | "gql"
+      url: string
+      options?: Record<string, unknown>
+    }
+  }
+*/
+
+/*export type ModuleResolution = {
+  resolutionPath: string | false
+  definition: ModuleDefinition
+  options?: Record<string, unknown>
+  moduleDeclaration?: ConfigurableModuleDeclaration
+}*/
+
+export type ModuleDefinition = {
+  key: string
+  registrationName: string
+  defaultPackage: string | false
+  label: string
+  canOverride?: boolean
+  isRequired?: boolean
+  defaultModuleDeclaration: ConfigurableModuleDeclaration
+}
+
+export type LoaderOptions = {
+  container: MedusaContainer
+  configModule: ConfigModule
+  options?: Record<string, unknown>
+  logger?: Logger
+}
+
 export type Constructor<T> = new (...args: any[]) => T
 
 export type ModuleExports = {
